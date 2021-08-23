b0d400385f8b48e7b2fceabd79dc754df2d140b8
VendraH <vhmnt123@gmail.com>
2021-08-23 19:57:02 +0530

    first maven project commit

---
+++ src/main/java/com/hemv/app/App.java
@@ -0,0 +1,13 @@
+package com.hemv.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
